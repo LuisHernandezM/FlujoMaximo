facbca8c4dbbe8d188d09cf3698d1bc6ec63055d
LuisHernandezM <luis.shdw.gen@outlook.es>
2017-12-09 22:24:55 -0600

    version funcional

--- src/Procesamiento/FlujoMaximo.java
+++ src/Procesamiento/FlujoMaximo.java
@@ -30,58 +30,71 @@ public class FlujoMaximo extends Thread{
     @Override
     public void run(){
         //Nodo inicial = listaNodos.get(0);
-        //Nodo fin = listaNodos.get(listaNodos.size()-1);
+        Nodo fin = listaNodos.get(listaNodos.size()-1);
         boolean bandera = true;
-        ArrayList<Integer> aux = new ArrayList<>();
-        ArrayList<Integer> faux = new ArrayList<>(); 
-        int flow = 0;
+        ArrayList<Integer> aux = new ArrayList<>(); //indices de nodos
+        ArrayList<Integer> faux = new ArrayList<>(); //indices de flechas
+        int flow = 0; //valor del flujo maximo
         while(true) {
-            aux.add(0);
+            aux.add(0); // agrega el nodo inicial
             while(bandera){
-                int max = -1;
+                int max = -1; // variable para obtener la flecha con el mayor valor
                 Nodo n = listaNodos.get(aux.get(aux.size()-1));
                 int iFlecha=-1;
                 int i=0;
                 for (Flecha flecha: listaFlechas){
-                    if(new Rectangle(flecha.getX1()-Nodo.d/2, flecha.getY1()-Nodo.d/2,Nodo.d,Nodo.d).contains(new Point(n.getX(),n.getY()))){
-                        if (flecha.getValor()>max){
-                            max = flecha.getValor();
-                            iFlecha = i;
-                        }
+                    if(new Rectangle(n.getX()-Nodo.d/2, n.getY()-Nodo.d/2,Nodo.d,Nodo.d).contains(new Point(flecha.getX1(),flecha.getY1()))){
+                        //if ()
+                        if (new Rectangle(fin.getX()-Nodo.d/2, fin.getY()-Nodo.d/2,Nodo.d,Nodo.d).contains(new Point(flecha.getX2(),flecha.getY2()))!=true){
+                            if (flecha.getValor()>max&&flecha.getValor()!=0){
+                                max = flecha.getValor();
+                                iFlecha = i;
+                                System.out.println(flecha.getValor());
+                            }
+                        }   
                     }
                     i++;
                 }
                 int jNodo=-1;
                 int j=0;
                 faux.add(iFlecha);
+                System.out.println(listaFlechas.get(faux.size()-1).getValor());
                 Flecha f = listaFlechas.get(faux.size()-1);
+                //Nodo ant = listaNodos.get(aux.get(aux.size()-1));
                 for (Nodo nodo: listaNodos){
                     if(new Rectangle(nodo.getX()-Nodo.d/2, nodo.getY()-Nodo.d/2,Nodo.d,Nodo.d).contains(new Point(f.getX2(),f.getY2()))){
-                        jNodo = j;
-                        aux.add(jNodo);
+                        if (nodo.equals(fin)){
+                            jNodo = j;
+                            aux.add(jNodo);
+                        }
+                        //
                     }
                     j++;
                 }
-                if ((listaNodos.size()-1)==jNodo){
+                if (listaNodos.size()-1==jNodo){
                     bandera = false;
                 }
             }
+            System.out.println();
+            // *****************************************************************
             int valmin = 999999;
             for (int t=0;t<faux.size();t++){
-                Flecha fle = listaFlechas.get(aux.get(t));
+                Flecha fle = listaFlechas.get(faux.get(t));
                 if(fle.getValor()<valmin){
                     valmin=fle.getValor();
                 }
             }
             for (int t=0;t<faux.size();t++){
-                Flecha fle = listaFlechas.get(aux.get(t));
-                fle.setValor(fle.getValor()-valmin);
-                grafo.repaint();
-                try {
-                    this.sleep(1000);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(FlujoMaximo.class.getName()).log(Level.SEVERE, null, ex);
+                Flecha fle = listaFlechas.get(faux.get(t));
+                if (fle.getValor()>=valmin){
+                    listaFlechas.get(faux.get(t)).setValor(fle.getValor()-valmin);
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(FlujoMaximo.class.getName()).log(Level.SEVERE, null, ex);
+                    }
                 }
+                grafo.repaint();
             }
             int acu=0;
             Nodo nodoAux = listaNodos.get(0);
